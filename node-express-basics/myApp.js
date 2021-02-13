24189643d42bf4f852172e9562554f652b01acea
jordanhailey <jordan.m.hailey@gmail.com>
2021-02-13 02:04:25 -0800

    lesson 4, adding middleware

--- node-express-basics/myApp.js
+++ node-express-basics/myApp.js
@@ -6,9 +6,16 @@ console.log("Hello World");
 app.get('/',(req,res)=>{
   // Lesson 2: All get requests to "/" receive "Hello Express" message
   // res.send("Hello Express")
+
   // Lesson 3: Send index.html in the ./views folder as response
   res.sendFile(`${__dirname}/views/index.html`,(err)=>{console.error(err)})
 })
+
+// Lesson 4: Adding Middleware
+// Route all requests to 'public/'
+// through 'express.static(<path>)' middleware
+const middlewareExample = express.static(`${__dirname}/public`)
+app.use('/public',middlewareExample)
 
 
 
