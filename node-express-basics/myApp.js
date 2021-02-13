795101c40ba18075c15663085e71555500c12e97
jordanhailey <jordan.m.hailey@gmail.com>
2021-02-13 02:12:14 -0800

    lesson 5, serve JSON

--- node-express-basics/myApp.js
+++ node-express-basics/myApp.js
@@ -5,19 +5,24 @@ console.log("Hello World");
 
 app.get('/',(req,res)=>{
   // Lesson 2: All get requests to "/" receive "Hello Express" message
-  // res.send("Hello Express")
+  // res.send("Hello Express");
 
   // Lesson 3: Send index.html in the ./views folder as response
-  res.sendFile(`${__dirname}/views/index.html`,(err)=>{console.error(err)})
+  res.sendFile(`${__dirname}/views/index.html`,(err)=>{console.error(err)});
 })
 
 // Lesson 4: Adding Middleware
-// Route all requests to 'public/'
+// Route all requests to '/public'
 // through 'express.static(<path>)' middleware
-const middlewareExample = express.static(`${__dirname}/public`)
-app.use('/public',middlewareExample)
+const middlewareExample = express.static(`${__dirname}/public`);
+app.use('/public',middlewareExample);
 
-
+// Lesson 5: Serve JSON
+// Respond to all GET requests to '/json' with a JSON object
+app.get('/json',(req,res)=>{
+  const responseObj = {"message":"Hello json"};
+  res.json(responseObj);
+})
 
 
 
