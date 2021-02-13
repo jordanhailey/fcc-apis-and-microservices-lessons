50be06af9ed141e54d9956c4628dc6964b8e999c
jordanhailey <jordan.m.hailey@gmail.com>
2021-02-13 01:36:54 -0800

    lesson 2, basic routing with express

--- node-express-basics/myApp.js
+++ node-express-basics/myApp.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+// Lesson 1: Print message to console
 console.log("Hello World");
 
-
+// Lesson 2: All get requests to "/" receive "Hello Express" message
+app.get('/',(req,res)=>{
+  res.send("Hello Express")
+})
 
 
 
